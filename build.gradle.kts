61e047233a6be207b681c42c5e67a2a66f01c274
hotaek <kht072623@naver.com>
2022-03-22 20:49:56 +0900

    #1 [CHORE] base_setting
    기초 세팅 완료!

---
+++ build.gradle.kts
@@ -0,0 +1,25 @@
+
+buildscript {
+    val kotlin_version by extra("1.5.0-release-764")
+    repositories {
+        google()
+        mavenCentral()
+        maven("https://plugins.gradle.org/m2/")
+    }
+    dependencies {
+        classpath("com.android.tools.build:gradle:7.1.2")
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.6.10")
+        classpath("com.google.gms:google-services:4.3.10")
+    }
+}
+allprojects {
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+
+tasks.register("clean",Delete::class) {
+    delete(rootProject.buildDir)
+}
