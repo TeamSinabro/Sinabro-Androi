0ec542b786778040e10229696b317d5d4cb087c2
hotaek <kht072623@naver.com>
2022-04-23 19:22:26 +0900

    #3 [UI] package_base_layout
    스피너 및 뷰 전체 적용 완료

--- app/src/main/java/com/sinabro/presentation/ui/subjectselect/SubjectSelectActivity.kt
+++ app/src/main/java/com/sinabro/presentation/ui/subjectselect/SubjectSelectActivity.kt
@@ -1,13 +1,45 @@
 package com.sinabro.presentation.ui.subjectselect
 
 import android.os.Bundle
+import android.view.View
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
+import android.widget.Spinner
 import com.sinabro.R
 import com.sinabro.databinding.ActivitySubjectSelectBinding
 import com.sinabro.presentation.base.BaseActivity
 
+
 class SubjectSelectActivity : BaseActivity<ActivitySubjectSelectBinding>(R.layout.activity_subject_select) {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        initSpinner()
+        checkSpinnerItem(binding.spinnerSubjectSelect)
+        checkSpinnerItem(binding.spinnerSubjectSelectUnit)
+    }
+
+    //스피너 생성
+    private fun initSpinner(){
+        binding.spinnerSubjectSelect.adapter = ArrayAdapter.createFromResource(this, R.array.subject_select,R.layout.item_spinner)
+        binding.spinnerSubjectSelectUnit.adapter = ArrayAdapter.createFromResource(this, R.array.subject_unit, R.layout.item_spinner)
+    }
+
+    //스피너 아이템 선택
+    private fun checkSpinnerItem(view : Spinner){
+        view.onItemSelectedListener = object: AdapterView.OnItemSelectedListener{
+            override fun onItemSelected(
+                parent: AdapterView<*>?,
+                view: View?,
+                position: Int,
+                id: Long
+            ) {
+
+            }
+
+            override fun onNothingSelected(parent: AdapterView<*>?) {
+
+            }
+        }
 
 
     }
--- app/src/main/java/com/sinabro/shared/di/SinaBroApplication.kt
+++ app/src/main/java/com/sinabro/shared/di/SinaBroApplication.kt
@@ -1,9 +1,8 @@
 package com.sinabro.shared.di
 
 import android.app.Application
-import dagger.hilt.android.HiltAndroidApp
 
-@HiltAndroidApp
+
 class SinaBroApplication : Application() {
     override fun onCreate() {
         super.onCreate()
