4170da286fbcb8b38f02ee3826c0e7a2d8ec40e3
hotaek <kht072623@naver.com>
2022-04-25 14:43:39 +0900

    #4 [UI] main_activity
    클릭 이벤트 처리 및 완료

--- app/src/main/java/com/sinabro/presentation/ui/main/MainActivity.kt
+++ app/src/main/java/com/sinabro/presentation/ui/main/MainActivity.kt
@@ -1,16 +1,51 @@
 package com.sinabro.presentation.ui.main
 
-import androidx.appcompat.app.AppCompatActivity
+import android.app.Activity
+import android.content.Intent
 import android.os.Bundle
 import com.sinabro.R
 import com.sinabro.databinding.ActivityMainBinding
 import com.sinabro.presentation.base.BaseActivity
+import com.sinabro.presentation.ui.pronouncelearning.PronounceLearningActivity
+import com.sinabro.presentation.ui.subjectselect.SubjectSelectActivity
+import com.sinabro.presentation.ui.vocalearning.VocaLearningActivity
+import com.sinabro.presentation.ui.vocasearch.VocaSearchActivity
+
 
 class MainActivity : BaseActivity<ActivityMainBinding>(R.layout.activity_main) {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        processGoView()
+
     }
+
+    //intent 부분
+    private fun clickView(activity : Activity){
+        val intent = Intent(this, activity::class.java)
+        startActivity(intent)
+    }
+
+    //클릭에 따른 분기처리
+    private fun processGoView(){
+        binding.imgSetting.setOnClickListener {
+            clickView(SubjectSelectActivity())
+        }
+        binding.textPronounceLearning.setOnClickListener {
+            clickView(PronounceLearningActivity())
+        }
+        binding.textVocaLearning.setOnClickListener {
+            clickView(VocaLearningActivity())
+        }
+        binding.textVocaSearch.setOnClickListener {
+            clickView(VocaSearchActivity())
+        }
+    }
+
+
+
+
+
 
 
 }
---
+++ app/src/main/java/com/sinabro/presentation/ui/main/viewmodel/MainViewModel.kt
@@ -0,0 +1,9 @@
+package com.sinabro.presentation.ui.main.viewmodel
+
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class MainViewModel : ViewModel() {
+
+
+}
