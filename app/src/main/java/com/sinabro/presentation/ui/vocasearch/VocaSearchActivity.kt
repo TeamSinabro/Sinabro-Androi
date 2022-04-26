572bcf9c93fff139c883d0be48f73c644495e6ba
hotaek <kht072623@naver.com>
2022-04-26 21:16:37 +0900

    #5 [FEAT] voca_search
    단어 검색 타이틀 부분 완성

--- app/src/main/java/com/sinabro/presentation/ui/vocasearch/VocaSearchActivity.kt
+++ app/src/main/java/com/sinabro/presentation/ui/vocasearch/VocaSearchActivity.kt
@@ -3,10 +3,12 @@ package com.sinabro.presentation.ui.vocasearch
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.sinabro.R
+import com.sinabro.databinding.ActivityVocaSearchBinding
+import com.sinabro.presentation.base.BaseActivity
 
-class VocaSearchActivity : AppCompatActivity() {
+class VocaSearchActivity : BaseActivity<ActivityVocaSearchBinding>(R.layout.activity_voca_search) {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_voca_search)
+
     }
 }
