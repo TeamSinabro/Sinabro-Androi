4347743021dcdedd3e129850419d95cbf8398209
hotaek <kht072623@naver.com>
2022-04-26 21:35:32 +0900

    #5 [FEAT] voca_search
    단어 검색 완료

--- app/src/main/java/com/sinabro/presentation/ui/pronouncelearning/PronounceLearningActivity.kt
+++ app/src/main/java/com/sinabro/presentation/ui/pronouncelearning/PronounceLearningActivity.kt
@@ -3,10 +3,12 @@ package com.sinabro.presentation.ui.pronouncelearning
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.sinabro.R
+import com.sinabro.databinding.ActivityPronounceLearningBinding
+import com.sinabro.presentation.base.BaseActivity
 
-class PronounceLearningActivity : AppCompatActivity() {
+class PronounceLearningActivity : BaseActivity<ActivityPronounceLearningBinding>(R.layout.activity_pronounce_learning) {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_pronounce_learning)
+
     }
 }
