d75f800d1d809ef8db8e50e0bba609891ae57690
hotaek <kht072623@naver.com>
2022-04-26 21:59:43 +0900

    #6 [FEAT] voca_search
    어휘 학습 완료

--- app/src/main/java/com/sinabro/presentation/ui/vocalearning/VocaLearningActivity.kt
+++ app/src/main/java/com/sinabro/presentation/ui/vocalearning/VocaLearningActivity.kt
@@ -1,12 +1,26 @@
 package com.sinabro.presentation.ui.vocalearning
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.sinabro.R
+import com.sinabro.databinding.ActivityVocaLearningBinding
+import com.sinabro.databinding.ActivityVocaSearchBinding
+import com.sinabro.presentation.base.BaseActivity
 
-class VocaLearningActivity : AppCompatActivity() {
+class VocaLearningActivity : BaseActivity<ActivityVocaLearningBinding>(R.layout.activity_voca_learning) {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_voca_learning)
+        goAnswer()
+    }
+
+    //정답 확인
+    private fun goAnswer(){
+        binding.textVocaLearningOk.setOnClickListener {
+            val intent = Intent(this, VocaLearningAnswerActivity::class.java)
+            startActivity(intent)
+            finish()
+        }
+
     }
 }
---
+++ app/src/main/java/com/sinabro/presentation/ui/vocalearning/VocaLearningAnswerActivity.kt
@@ -0,0 +1,25 @@
+package com.sinabro.presentation.ui.vocalearning
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.sinabro.R
+import com.sinabro.databinding.ActivityVocaLearningAnswerBinding
+import com.sinabro.presentation.base.BaseActivity
+
+class VocaLearningAnswerActivity : BaseActivity<ActivityVocaLearningAnswerBinding>(R.layout.activity_voca_learning_answer) {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        goMain()
+    }
+
+
+    //메인 이동
+    private fun goMain(){
+        binding.textVocaLearningMain.setOnClickListener {
+            finish()
+        }
+    }
+
+    //다음 문제제
+}
