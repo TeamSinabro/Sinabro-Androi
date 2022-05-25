b843997826f11dbf5c9bfffc01a5ad813da581f3
hotaek <kht072623@naver.com>
2022-05-25 16:04:08 +0900

    #17 [FEAT] onboarding
    출판사 선택으로 넘어가게끔 설정

--- app/src/main/java/com/sinabro/presentation/ui/onboarding/OnBoardingActivity.kt
+++ app/src/main/java/com/sinabro/presentation/ui/onboarding/OnBoardingActivity.kt
@@ -1,17 +1,20 @@
 package com.sinabro.presentation.ui.onboarding
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.sinabro.R
 import com.sinabro.databinding.ActivityOnBoardingBinding
 import com.sinabro.presentation.base.BaseActivity
 import com.sinabro.presentation.ui.onboarding.adapter.OnBoardingAdapter
+import com.sinabro.presentation.ui.subjectselect.SubjectSelectActivity
 
 class OnBoardingActivity : BaseActivity<ActivityOnBoardingBinding>(R.layout.activity_on_boarding) {
     private lateinit var onBoardingAdapter : OnBoardingAdapter
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         initView()
+        goSubjectSelect()
     }
 
 
@@ -20,5 +23,14 @@ class OnBoardingActivity : BaseActivity<ActivityOnBoardingBinding>(R.layout.acti
         onBoardingAdapter = OnBoardingAdapter(this)
         binding.vpOnboarding.adapter = onBoardingAdapter
         binding.dotsIndicator.setViewPager2(binding.vpOnboarding)
+    }
+
+    //출판사 선택으로 넘어가기
+    private fun goSubjectSelect(){
+        binding.textStart.setOnClickListener {
+            val intent= Intent(this, SubjectSelectActivity::class.java)
+            startActivity(intent)
+            finish()
+        }
     }
 }
