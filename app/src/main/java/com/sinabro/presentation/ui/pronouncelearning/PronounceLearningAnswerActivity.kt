4788e11ce527e6ae5660e87c2be38d4324e5ea71
hotaek <kht072623@naver.com>
2022-05-30 00:28:21 +0900

    #19 [FIX] pronounce_learning_update
    발음학습 수정

--- app/src/main/java/com/sinabro/presentation/ui/pronouncelearning/PronounceLearningAnswerActivity.kt
+++ app/src/main/java/com/sinabro/presentation/ui/pronouncelearning/PronounceLearningAnswerActivity.kt
@@ -1,17 +1,20 @@
 package com.sinabro.presentation.ui.pronouncelearning
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.sinabro.R
 import com.sinabro.databinding.ActivityPronounceLearningAnswerBinding
 import com.sinabro.databinding.ActivityPronounceLearningBinding
 import com.sinabro.presentation.base.BaseActivity
+import com.sinabro.shared.util.SinabroShareData
 
 class PronounceLearningAnswerActivity : BaseActivity<ActivityPronounceLearningAnswerBinding>(R.layout.activity_pronounce_learning_answer) {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         goMain()
         initScore()
+        nextAnswer()
     }
 
     //메인으로 이동
@@ -22,11 +25,23 @@ class PronounceLearningAnswerActivity : BaseActivity<ActivityPronounceLearningAn
     }
     //점수 받아오기
     private fun initScore(){
-        val score = intent.getStringExtra("pronounceScore")
-        val dNum = ((score?.toDouble()?.times(20) ?:0 ))
-        binding.textPronounceLearningScore.text = String.format("%2.f", dNum)
+        val score = intent.getStringExtra("pronounceScore")?.toDouble()
+        val dNum = ((score?.times(20)))
+        binding.textPronounceLearningScore.text = String.format("%.2f", dNum)
 
 
     }
     //다음문제
+    private fun nextAnswer(){
+        if(SinabroShareData.answerNum != 6){
+            finish()
+        }else{
+            val intent = Intent(this, PronounceLearningActivity::class.java)
+            startActivity(intent)
+            finish()
+
+        }
+
+
+    }
 }
