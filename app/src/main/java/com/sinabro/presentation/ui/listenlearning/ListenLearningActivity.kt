c92739609e573052bd7bc18c2045551da8b8357c
hotaek <kht072623@naver.com>
2022-06-01 14:32:42 +0900

    #24 [FEAT] listen_learning_tts
    문장 데이터 받아오기 서버 통신 완료

--- app/src/main/java/com/sinabro/presentation/ui/listenlearning/ListenLearningActivity.kt
+++ app/src/main/java/com/sinabro/presentation/ui/listenlearning/ListenLearningActivity.kt
@@ -7,6 +7,7 @@ import com.sinabro.R
 import com.sinabro.databinding.ActivityListenLearningBinding
 import com.sinabro.presentation.base.BaseActivity
 import com.sinabro.presentation.ui.listenlearning.viewmodel.ListenLearningViewModel
+import com.sinabro.shared.util.SinabroShareData
 import dagger.hilt.android.AndroidEntryPoint
 import org.koin.androidx.viewmodel.ext.android.viewModel
 
@@ -17,7 +18,22 @@ class ListenLearningActivity : BaseActivity<ActivityListenLearningBinding>(R.lay
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        initSentenceData()
+        checkLoading()
+    }
 
 
+    //문장 데이터 받아오기
+    private fun initSentenceData(){
+        val data = SinabroShareData
+        listenLearningViewModel.getSentenceData(data.publisher, data.subject, data.chapter)
+    }
+
+
+    //로딩바
+    private fun checkLoading(){
+        listenLearningViewModel.onLoadingEnd.observe(this){
+            dismissLoading()
+        }
     }
 }
--- app/src/main/java/com/sinabro/presentation/ui/listenlearning/viewmodel/ListenLearningViewModel.kt
+++ app/src/main/java/com/sinabro/presentation/ui/listenlearning/viewmodel/ListenLearningViewModel.kt
@@ -1,13 +1,46 @@
 package com.sinabro.presentation.ui.listenlearning.viewmodel
 
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.sinabro.domain.usecase.pronounce.GetPronounceSentenceDataUseCase
+import com.sinabro.presentation.base.LoadedViewModel
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.launch
+import timber.log.Timber
 import javax.inject.Inject
 
 @HiltViewModel
 class ListenLearningViewModel @Inject constructor(
     private val getPronounceSentenceDataUseCase : GetPronounceSentenceDataUseCase
-) : ViewModel() {
+) : ViewModel(), LoadedViewModel {
+
+    //듣기 학습 문장 데이터
+    var sentence = MutableLiveData<String>()
+
+
+
+    //문장 데이터 받아오기
+    fun getSentenceData(publisher : String, subject : String, chapter : Int ){
+
+        viewModelScope.launch {
+            runCatching { getPronounceSentenceDataUseCase(publisher, subject, chapter) }
+                .onSuccess {
+                    sentence.value = it.problem
+                    Timber.d("듣기 학습 문장 받아오기 완료")
+                }
+                .onFailure {
+                    Timber.d("듣기 학습 문장 받아오기 실패")
+                }.also {
+                    onLoadingEnd.value = true
+                }
+
+
+        }
+
+
+    }
+
+    override val onLoadingEnd =MutableLiveData<Boolean>()
 
 }
