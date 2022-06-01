12e71e5d50c33f85ba14ff9d977029c6e53a7785
hotaek <kht072623@naver.com>
2022-06-01 14:15:19 +0900

    #23 [FEAT] listen_learning_stt
    activity 생성

---
+++ app/src/main/java/com/sinabro/presentation/ui/listenlearning/ListenLearningActivity.kt
@@ -0,0 +1,14 @@
+package com.sinabro.presentation.ui.listenlearning
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.sinabro.R
+import com.sinabro.databinding.ActivityListenLearningBinding
+import com.sinabro.presentation.base.BaseActivity
+
+class ListenLearningActivity : BaseActivity<ActivityListenLearningBinding>(R.layout.activity_listen_learning) {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+    }
+}
