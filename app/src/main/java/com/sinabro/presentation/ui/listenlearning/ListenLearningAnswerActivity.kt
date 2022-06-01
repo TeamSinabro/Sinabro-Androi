1f2d99ebe9c3123f9223ea5966d1fab054643ff1
hotaek <kht072623@naver.com>
2022-06-01 14:55:21 +0900

    #24 [FEAT] listen_learning_tts
    버튼 이벤트 활성화

--- app/src/main/java/com/sinabro/presentation/ui/listenlearning/ListenLearningAnswerActivity.kt
+++ app/src/main/java/com/sinabro/presentation/ui/listenlearning/ListenLearningAnswerActivity.kt
@@ -1,5 +1,6 @@
 package com.sinabro.presentation.ui.listenlearning
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.sinabro.R
@@ -10,13 +11,29 @@ class ListenLearningAnswerActivity : BaseActivity<ActivityListenLearningAnswerBi
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         initView()
+        checkEvent()
     }
 
 
+    //뷰 및 데이터 연결
     private fun initView(){
         val answer = intent.getStringExtra("answer")
         val userAnswer = intent.getStringExtra("userAnswer")
         binding.textListenUserAnswer.text = userAnswer
         binding.textListenAnswer.text = answer
+    }
+
+    //클릭 이벤트
+    private fun checkEvent(){
+        binding.textListenLearningMain.setOnClickListener {
+            finish()
+        }
+        binding.textListenLearningNext.setOnClickListener {
+            val intent = Intent(this, ListenLearningActivity::class.java)
+            startActivity(intent)
+            finish()
+
+        }
+
     }
 }
